4daad9dbf8351f554b4178c0629904ea56add14a
edstarck <edstarck@users.noreply.github.com>
2023-05-05 14:48:16 +0300

    done count neighbors

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ const state = {
   gridWidth: null,
   gridHeight: null,
   cellSize: 20,
+  requestId: null,
+  gameInterval: null,
   canvas: document.createElement('canvas'),
   getCtx() {
     return this.canvas.getContext('2d')
@@ -35,6 +37,30 @@ const methods = {
       }
     }
   },
+  getNeighborCount(x, y) {
+    let count = 0
+    //   1 0 1
+    //   1 I(0,0) 0
+    //   0 0 1
+
+    //   sum += grid[i - 1][j - 1]
+    //   sum += grid[i][j - 1]
+    //   sum += grid[i + 1][j - 1]
+    //   etc......
+
+    for (let dx = -1; dx <= 1; dx++) {
+      for (let dy = -1; dy <= 1; dy++) {
+        // exclude myself from neighbors
+        if (dx === 0 && dy === 0) {
+          continue
+        }
+        let nx = (x + dx + state.gridWidth) % state.gridWidth
+        let ny = (y + dy + state.gridHeight) % state.gridHeight
+        count += state.grid[nx][ny]
+      }
+    }
+    return count
+  },
 }
 
 const mutation = {
@@ -48,14 +74,40 @@ const mutation = {
     ctx.fillRect(0, 0, state.canvas.width, state.canvas.height)
     document.getElementById(el).appendChild(state.canvas)
 
-    state.gridWidth = state.canvas.width / state.cellSize
-    state.gridHeight = state.canvas.height / state.cellSize
+    state.gridWidth = Math.ceil(state.canvas.width / state.cellSize)
+    state.gridHeight = Math.ceil(state.canvas.height / state.cellSize)
   },
   createEmptyGrid() {
     state.grid = new Array(state.gridWidth)
     for (let i = 0; i < state.gridWidth; i++) {
       state.grid[i] = new Array(state.gridHeight).fill(0)
     }
+  },
+  updateGrid() {
+    const {drawGrid, getNeighborCount} = methods
+
+    let newGrid = new Array(state.gridWidth)
+    for (let i = 0; i < state.gridWidth; i++) {
+      newGrid[i] = new Array(state.gridHeight).fill(0)
+    }
+
+    for (let x = 0; x < state.gridWidth; x++) {
+      for (let y = 0; y < state.gridHeight; y++) {
+        let neighbors = getNeighborCount(x, y)
+        if (state.grid[x][y] === 1 && (neighbors < 2 || neighbors > 3)) {
+          newGrid[x][y] = 0
+        } else if (state.grid[x][y] === 0 && neighbors === 3) {
+          newGrid[x][y] = 1
+        } else {
+          newGrid[x][y] = state.grid[x][y]
+        }
+      }
+    }
+
+    state.grid = newGrid
+    drawGrid()
+
+    // state.requestId = requestAnimationFrame(mutation.updateGrid)
   },
 }
 
@@ -81,11 +133,32 @@ state.canvas.addEventListener('click', function (event) {
 
 // Actions UI
 document.getElementById('start-btn').addEventListener('click', function () {
-  console.log('start')
+  const {updateGrid} = mutation
+  state.gameInterval = setInterval(updateGrid, 100)
+})
+document.getElementById('pause-btn').addEventListener('click', function () {
+  //  cancelAnimationFrame(state.requestId)
+  clearInterval(state.gameInterval)
 })
 document.getElementById('clear-btn').addEventListener('click', function () {
-  console.log('clear')
+  const {drawGrid} = methods
+
+  clearInterval(state.gameInterval)
+
+  for (let i = 0; i < state.gridWidth; i++) {
+    state.grid[i].fill(0)
+  }
+
+  drawGrid()
 })
 document.getElementById('random-btn').addEventListener('click', function () {
-  console.log('random')
+  const {drawGrid} = methods
+
+  for (let x = 0; x < state.gridWidth; x++) {
+    for (let y = 0; y < state.gridHeight; y++) {
+      state.grid[x][y] = Math.random() < 0.5 ? 1 : 0
+    }
+  }
+
+  drawGrid() // create a random initial generation
 })
